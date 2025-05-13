90fbcb22b2cad9c33eafc5c6ca61abd7718918f6
m6rc0sp <paulo.marcos0108@live.com>
2025-05-13 12:24:46 -0300

    feat: adiciona tipos para Node e ajusta configuração do Vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -33,11 +38,12 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://206.189.224.24:10000',
+        target: 'http://localhost:10000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false,
       }
-    }
+    },
+    allowedHosts: ['assinaturas.appns.com.br']
   }
 });
