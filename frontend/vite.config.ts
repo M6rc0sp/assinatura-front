072d03c1bab274ee31c2d6dfc1e3d369498e9087
m6rc0sp <paulo.marcos0108@live.com>
2025-05-13 16:32:15 -0300

    feat: define base URL for Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,6 +11,7 @@ const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ns/',
   optimizeDeps: {
     include: [
       '@nimbus-ds/patterns',
