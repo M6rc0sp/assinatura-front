464590b0ae2a98265493978a68d5899f4c85afa1
m6rc0sp <paulo.marcos0108@live.com>
2026-03-12 15:50:49 -0300

    fix: altera a base da configuração do Vite para o caminho raiz

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ns/',
+  base: '/',
   optimizeDeps: {
     include: [
       '@nimbus-ds/patterns',
